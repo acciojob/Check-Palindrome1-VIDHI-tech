443f70e398bf9697fdade6cbf25adcd95b4d2499
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-04 19:21:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(s){
+	// Step 1: Convert the string to lowercase
+  s = s.toLowerCase();
 
+  // Step 2: Remove non-alphanumeric characters
+  s = s.replace(/[^a-z0-9]/g, '');
+
+  // Step 3: Check if the string is equal to its reverse
+  return s === s.split('').reverse().join('');
 }
 module.exports = palindrome
